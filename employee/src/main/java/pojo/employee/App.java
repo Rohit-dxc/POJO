c9b699377a1bba588ecc8334f11aa368d4754e1a
Rohit-dxc <rrohit3@dxc.com>
2019-12-11 11:12:36 +0530

    Initial commit.

---
+++ employee/src/main/java/pojo/employee/App.java
@@ -0,0 +1,31 @@
+package pojo.employee;
+
+import java.io.File;
+//import javax.xml.bind.JAXBContext;
+//import javax.xml.bind.JAXBException;
+//import javax.xml.bind.Unmarshaller;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
+import pojo.employee.emp.Customer;
+
+public class App {
+	public static void main(String[] args) {
+
+		try {
+
+			File file = new File("C:\\Users\\rrohit3\\Desktop\\ADData.xml");
+			JAXBContext jaxbContext = JAXBContext.newInstance(Customer.class);
+
+			Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+			Customer customer = (Customer) jaxbUnmarshaller.unmarshal(file);
+			System.out.println(customer);
+
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
